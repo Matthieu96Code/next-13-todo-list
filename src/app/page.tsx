f45ed9571e478613b705a8556ada8bd01f5369f9
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-03 10:43:26 +0000

    Add a header and the link to the new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+// import Link from "../../node_modules/next/link"
+import Link from "next/link"
+
 export default function Home() {
-  return <h1>Hi</h1>
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+      </header>
+      <ul>
+      </ul>
+    </>
+  )
 }
