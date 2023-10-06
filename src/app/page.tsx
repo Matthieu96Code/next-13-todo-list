30c395533dd3f05cba609a1c4a4fcefd25d1b66d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-06 23:25:20 +0000

    Add todo and map throught

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 // import Link from "../../node_modules/next/link"
+import { prisma } from "@/db"
 import Link from "next/link"
 
-export default function Home() {
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
+  // await prisma.todo.create({ data: { title: "test", complete: false} })
+
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
         <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
       </header>
-      <ul>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
       </ul>
     </>
   )
