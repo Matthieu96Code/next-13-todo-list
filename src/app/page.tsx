918b0ff24efdacd09562780275bcd348c20af124
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-06 23:39:20 +0000

    Add todo Item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // import Link from "../../node_modules/next/link"
+import { TodoItem } from "@/components/TodoItem"
 import { prisma } from "@/db"
 import Link from "next/link"
 
@@ -18,7 +19,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(todo => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoItem key={todo.id} {...todo} />
         ))}
       </ul>
     </>
