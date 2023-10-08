b0a0c754e7d76cef55f19381a3c8599bf76f56f8
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-08 03:08:19 +0000

    Add an input form to the new page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,18 @@
+import Link from "../../../node_modules/next/link"
+
 export default function Page() {
-    return <h1>New</h1>
+    return (
+        <>
+            <header className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl">New</h1>
+            </header>
+            <form className="flex gap-2 flex-col">
+                <input
+                    type="text"
+                    name="title" 
+                    className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+                />
+            </form>
+        </>
+    )
 }
