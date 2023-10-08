f1671eefd793f29960bd7ab4145298580d065992
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-08 02:37:32 +0000

    Implement list behavior when checked

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,5 +5,10 @@ type TodoItemProps = {
 }
 
 export function TodoItem({ id, title, complete }: TodoItemProps) {
-
+    return <li className="flex gap-1 items-center">
+        <input id={id} type="checkbox" className="cursor-pointer peer"/>
+        <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
+            {title}
+        </label>
+    </li>
 }
